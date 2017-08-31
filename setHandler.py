9d70f8c4aec7ae89476af3b21664126265abee1a
Marco ma <mahomehi@126.com>
2017-08-30 21:48:18 -0700

    1. Simplify the logic of setHandler.

--- setHandler.py
+++ setHandler.py
@@ -32,12 +32,7 @@ class setHandler:
         if(self.isValidType(dbName) and self.isValidType(setName)):
             if(self.database.isSetExist(dbName, setName) is False):
                 result = self.database.createSet(dbName, setName)
-                if(result == responseCode.SET_CREATE_SUCCESS):
-                    msg = self.makeMessage("Set Create Success", responseCode.SET_CREATE_SUCCESS, setName)
-                elif(result == responseCode.KEY_NAME_INVALID):
-                    msg = self.makeMessage("Set Name Is Invalid", responseCode.KEY_NAME_INVALID, setName)
-                else:
-                    msg = self.makeMessage("Database Error", responseCode.DB_ERROR, dbName)
+                msg = self.makeMessage(responseCode.detail[result],result,setName)
             else:
                 msg = self.makeMessage("Set Already Exists", responseCode.SET_ALREADY_EXIST, setName)
         else:
@@ -65,14 +60,7 @@ class setHandler:
             if(self.database.isSetExist(dbName, setName)):
                 if(self.database.isExpired(dbName, setName, "SET") is False):
                     result = self.database.insertSet(dbName, setName, setValue)
-                    if(result == responseCode.SET_IS_LOCKED):
-                        msg = self.makeMessage("Set Is Locked", responseCode.SET_IS_LOCKED, setName)
-                    elif(result == responseCode.SET_VALUE_ALREADY_EXIST):
-                        msg = self.makeMessage("Set Value Already Exists", responseCode.SET_VALUE_ALREADY_EXIST, setValue)
-                    elif(result == responseCode.SET_INSERT_SUCCESS):
-                        msg = self.makeMessage("Set Insert Success", responseCode.SET_INSERT_SUCCESS, setName)
-                    else:
-                        msg = self.makeMessage("Database Error", responseCode.DB_ERROR, dbName)
+                    msg = self.makeMessage(responseCode.detail[result], result, setName)
                 else:
                     msg = self.makeMessage("Set Is Expired", responseCode.SET_EXPIRED, setName)
             else:
@@ -87,14 +75,7 @@ class setHandler:
             if(self.database.isSetExist(dbName, setName)):
                 if(self.database.isExpired(dbName, setName, "SET") is False):
                     result = self.database.rmFromSet(dbName, setName, setValue)
-                    if(result == responseCode.SET_IS_LOCKED):
-                        msg = self.makeMessage("Set Is Locked", responseCode.SET_IS_LOCKED, setName)
-                    elif(result == responseCode.SET_VALUE_NOT_EXIST):
-                        msg = self.makeMessage("Set Value Does Not Exist", responseCode.SET_VALUE_NOT_EXIST, setValue)
-                    elif(result == responseCode.SET_REMOVE_SUCCESS):
-                        msg = self.makeMessage("Set Remove Success", responseCode.SET_REMOVE_SUCCESS, setValue)
-                    else:
-                        msg = self.makeMessage("Database Error", responseCode.DB_ERROR, dbName)
+                    msg = self.makeMessage(responseCode.detail[result], result, setName)
                 else:
                     msg = self.makeMessage("Set Is Expired", responseCode.SET_EXPIRED, setName)
             else:
@@ -109,12 +90,7 @@ class setHandler:
             if(self.database.isSetExist(dbName, setName)):
                 if(self.database.isExpired(dbName, setName, "SET") is False):
                     result = self.database.clearSet(dbName, setName)
-                    if(result == responseCode.SET_IS_LOCKED):
-                        msg = self.makeMessage("Set Is Locked", responseCode.SET_IS_LOCKED, setName)
-                    elif(result == responseCode.SET_CLEAR_SUCCESS):
-                        msg = self.makeMessage("Set Clear Success", responseCode.SET_CLEAR_SUCCESS, setName)
-                    else:
-                        msg = self.makeMessage("Database Error", responseCode.DB_ERROR, dbName)
+                    msg = self.makeMessage(responseCode.detail[result], result, setName)
                 else:
                     msg = self.makeMessage("Set Is Expired", responseCode.SET_EXPIRED, setName)
             else:
@@ -129,12 +105,7 @@ class setHandler:
             if (self.database.isSetExist(dbName, setName)):
                 if(self.database.isExpired(dbName, setName, "SET") is False):
                     result = self.database.deleteSet(dbName, setName)
-                    if (result == responseCode.SET_IS_LOCKED):
-                        msg = self.makeMessage("Set Is Locked", responseCode.SET_IS_LOCKED, setName)
-                    elif (result == responseCode.SET_DELETE_SUCCESS):
-                        msg = self.makeMessage("Set Delete Success", responseCode.SET_DELETE_SUCCESS, setName)
-                    else:
-                        msg = self.makeMessage("Database Error", responseCode.DB_ERROR, dbName)
+                    msg = self.makeMessage(responseCode.detail[result], result, setName)
                 else:
                     msg = self.makeMessage("Set Is Expired", responseCode.SET_EXPIRED, setName)
             else:
@@ -244,12 +215,7 @@ class setHandler:
             if (self.database.isSetExist(dbName, setName) is True):
                 if(self.database.isExpired(dbName, setName, "SET") is False):
                     result = self.database.replaceSet(dbName, setName, setValue)
-                    if (result == responseCode.SET_IS_LOCKED):
-                        msg = self.makeMessage("Set Is Locked", responseCode.SET_IS_LOCKED, setName)
-                    elif (result == responseCode.SET_REPLACE_SUCCESS):
-                        msg = self.makeMessage("Set Replace Success", responseCode.SET_REPLACE_SUCCESS, setName)
-                    else:
-                        msg = self.makeMessage("Database Error", responseCode.DB_ERROR, dbName)
+                    msg = self.makeMessage(responseCode.detail[result], result, setName)
                 else:
                     msg = self.makeMessage("Set Is Expired", responseCode.SET_EXPIRED, setName)
             else:
@@ -265,12 +231,7 @@ class setHandler:
                 msg = self.makeMessage("Set Does Not Exist", responseCode.SET_NOT_EXIST, setName)
             else:
                 result = self.database.setSetTTL(dbName, setName, ttl)
-                if (result == responseCode.SET_IS_LOCKED):
-                    msg = self.makeMessage("Set Is Locked", responseCode.SET_IS_LOCKED, setName)
-                elif (result == responseCode.SET_TTL_SET_SUCCESS):
-                    msg = self.makeMessage("Set TTL Set Success", responseCode.SET_TTL_SET_SUCCESS, setName)
-                else:
-                    msg = self.makeMessage("Database Error", responseCode.DB_ERROR, dbName)
+                msg = self.makeMessage(responseCode.detail[result], result, setName)
         else:
             msg = self.makeMessage("Element Type Error", responseCode.ELEM_TYPE_ERROR, setName)
         return msg
@@ -282,13 +243,7 @@ class setHandler:
                 msg = self.makeMessage("Set Does Not Exist", responseCode.SET_NOT_EXIST, setName)
             else:
                 result = self.database.clearSetTTL(dbName, setName)
-                if (result == responseCode.SET_IS_LOCKED):
-                    msg = self.makeMessage("Set Is Locked", responseCode.SET_IS_LOCKED, setName)
-                elif (result == responseCode.SET_TTL_CLEAR_SUCCESS):
-                    msg = self.makeMessage("Set TTL Clear Success", responseCode.SET_TTL_CLEAR_SUCCESS,
-                                           setName)
-                else:
-                    msg = self.makeMessage("Database Error", responseCode.DB_ERROR, dbName)
+                msg = self.makeMessage(responseCode.detail[result], result, setName)
         else:
             msg = self.makeMessage("Element Type Error", responseCode.ELEM_TYPE_ERROR, setName)
         return msg
@@ -296,13 +251,8 @@ class setHandler:
     def showTTL(self, dbName, keyName):
         if(self.isValidType(dbName) and self.isValidType(keyName)):
             if(self.database.isDbExist(dbName)):
-                result = self.database.showTTL(dbName, keyName, "SET")
-                if(result == responseCode.TTL_NO_RECORD):
-                    msg = self.makeMessage("TTL No Record", responseCode.TTL_NO_RECORD, keyName)
-                elif(result == responseCode.TTL_EXPIRED):
-                    msg = self.makeMessage("Set TTL Expired", responseCode.SET_EXPIRED, keyName)
-                else:
-                    msg = self.makeMessage("TTL Show Success", responseCode.TTL_SHOW_SUCCESS, result)
+                code, result = self.database.showTTL(dbName, keyName, "SET")
+                msg = self.makeMessage(responseCode.detail[code], code, result)
             else:
                 msg = self.makeMessage("Database Does Not Exist", responseCode.DB_NOT_EXIST, dbName)
         else:
